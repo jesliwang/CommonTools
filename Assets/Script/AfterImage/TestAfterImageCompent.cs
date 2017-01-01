01e5e7cca1ecb4550b29878d127fc31d14c65779
王剑虹 <smileking67@gmail.com>
2017-01-01 22:27:19 +0800

    base init with AfterImageComponent

---
+++ Assets/Script/AfterImage/TestAfterImageCompent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AfterImageComponent))]
+public class TestAfterImageCompent : MonoBehaviour {
+
+	protected AfterImageComponent afterImage;
+
+	protected Rigidbody physic;
+
+	// Use this for initialization
+	void Awake () {
+		afterImage = GetComponent<AfterImageComponent>();
+		physic = GetComponent<Rigidbody>();
+
+		afterImage.Play(10.0f, 1.0f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		physic.AddForce(new Vector3(1,0), ForceMode.Force);
+	}
+}
